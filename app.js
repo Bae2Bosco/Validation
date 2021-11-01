435a5c5592522056448e0c1f5d877ebb382262f5
Bae2Bosco <89792104+Bae2Bosco@users.noreply.github.com>
2021-10-31 23:59:51 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -2,24 +2,82 @@ var responseDiv=document.body.querySelector(".response");
 var list=[];
 
 document.body.querySelector(".butt").addEventListener("click", function(){
-    var textValue = document.body.querySelector(".input").value;
-    var textValuePass = document.body.querySelector(".inputPass").value;
-    if(textValue==="coolStudent123"&&textValuePass==="password"){
-        responseDiv.innerHTML="You are the Right User";
-        list.push(textValue);
-        window.location.href="https://bae2bosco.github.io/MenusInApp/";
-        renderList();
-    }else{
-        responseDiv.innerHTML="You didn't type in valid username"
-    }
-})
 
-function  renderList(){
-    var itemsDiv = document.body.querySelector(".items");
-    itemsDiv.innerHTML="";
-    for (var i=0; i<list.length; i++){
-        var ele=document.createElement("div");
-        ele.innerHTML=list[i];
-        itemsDiv.appendChild(ele);
+var pages = ["home", "about","view"];
+function createNav() {
+    var nav = document.createElement("nav");
+    createButton(pages[0]);
+    createButton(pages[1]);
+    createButton(pages[2]);
+
+    document.body.appendChild(nav);
+
+    function createButton(pg) {
+        var butt = document.createElement("button");
+        butt.innerHTML = pg;
+        butt.addEventListener("click", function () {
+            navigate(pg);
+        })
+        nav.appendChild(butt);
     }
 }
+
+function createWrapper(){
+    var wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+    document.body.appendChild(wrapper);
+
+}
+
+function navigate(pg){
+    var wrapper = document.body.querySelector(".wrapper");
+    if (pg === "home") {
+        homePage()
+    }
+    if (pg === "about") {
+        aboutPage()
+    }
+    if (pg === "view") {
+        intPage()
+        document.getElementById('counter_label').innerHTML = 'Counter: ' + counter;
+    }
+}
+
+
+
+function homePage(){
+    var wrapper=document.body.querySelector(".wrapper");
+    wrapper.innerHTML="";
+    var header = document.createElement("h1");
+    header.innerHTML="Home";
+    wrapper.appendChild(header);
+}
+
+function aboutPage(){
+    var wrapper=document.body.querySelector(".wrapper");
+    wrapper.innerHTML="";
+    var header = document.createElement("h1");
+    header.innerHTML="About";
+    header.innerHTML="Bae";
+    wrapper.appendChild(header);
+}
+
+function intPage(){
+    document.body.querySelector(".view-note").addEventListener("click", function(event){
+        var textValue = document.body.querySelector(".inputTxt").value;
+        var textValuePass = document.body.querySelector(".inputImp").value;
+        if(inputTxt)
+        var wrapper=document.body.querySelector(".wrapper");
+        wrapper.innerHTML="";
+        var header = document.createElement("h1");
+        header.innerHTML="View Notes";
+        wrapper.appendChild(header);
+    }
+}
+
+
+
+
+createNav();
+createWrapper();
+
